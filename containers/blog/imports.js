4ccaca01711d00a7bc88e04d26af10491502f9fe
Subash Jirel <subashjrl@gmail.com>
2024-02-27 18:17:40 +0545

    added the blog and article section

---
+++ containers/blog/imports.js
@@ -0,0 +1,7 @@
+import blog01 from '../../src/assets/blog01.png';
+import blog02 from '../../src/assets/blog02.png';
+import blog03 from '../../src/assets/blog03.png';
+import blog04 from '../../src/assets/blog04.png';
+import blog05 from '../../src/assets/blog05.png';
+
+export { blog01, blog02, blog03, blog04, blog05 };
